736be50b8f78affd389f3ccc68c5501c1d814c54
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-28 11:57:35 -1000

    remote repo updates

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
+const mysql = require('mysql')
 require('dotenv').config();
-const Sequelize = require('sequelize');
-const sequelize = process.env.JAWSDB_URL ?
-    new Sequelize(process.env.JAWSDB_URL) :
-    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      dialectOptions: {
-          decimalNumbers: true,
-      },
-  });
-
-
+const connectionLog = mysql.createPool({
+    connectionLimit : 10,
+    host: process.env.DB_HOST_LOG,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port:process.env.DB_PORT
+})
 
 module.exports = sequelize;
